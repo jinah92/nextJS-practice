5a4d2a97a71bdf1259de11627cd95d135bcbae1e
jinah <jinah@jinahui-MacBookPro.local>
2022-05-27 10:16:11 +0900

    new prj: simple movie app

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import Seo from '../components/Seo';
+export default function About() {
+  return (
+    <>
+      <Seo title={'About'} />
+      <h2>About page</h2>
+    </>
+  );
+}
